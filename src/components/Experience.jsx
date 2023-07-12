8b742833f5b14e03e530c81a64f7974f88bf2790
Djouko <destructeurkratos@gmail.com>
2023-07-12 09:50:24 +0100

    third

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -61,7 +61,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
+          What we have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
           Work Experience.
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,9 +80,9 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          Following projects showcases our skills and experience through
+          real-world examples of our work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects our
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
